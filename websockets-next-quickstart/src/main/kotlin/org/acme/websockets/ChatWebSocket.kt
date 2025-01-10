eeae9dc47731d39719d8dd900bc92a77ea303562
fujita_kyohei <fujita_kyohei@trial-net.co.jp>
2025-01-10 10:35:58 +0900

    feat: チャット用クラスを追加。まだチュートリアルのJavaコードを機械的にKotlinに置き換えただけで、コンパイルが通っていない

---
+++ websockets-next-quickstart/src/main/kotlin/org/acme/websockets/ChatWebSocket.kt
@@ -0,0 +1,38 @@
+package org.acme.websockets
+
+import io.quarkus.websockets.next.OnClose
+import io.quarkus.websockets.next.OnOpen
+import io.quarkus.websockets.next.OnTextMessage
+import io.quarkus.websockets.next.WebSocket
+import io.quarkus.websockets.next.WebSocketConnection
+import jakarta.inject.Inject
+
+@WebSocket(path = "/chat/{username}")
+class ChatWebSocket {
+    // Declare the type of messages that can be sent and received
+    enum class MessageType {
+        USER_JOINED, USER_LEFT, CHAT_MESSAGE
+    }
+
+    @JvmRecord
+    data class ChatMessage(val type: MessageType, val from: String, val message: String?)
+
+    @Inject
+    var connection: WebSocketConnection? = null
+
+    @OnOpen(broadcast = true)
+    fun onOpen(): ChatMessage {
+        return ChatMessage(MessageType.USER_JOINED, connection.pathParam("username"), null)
+    }
+
+    @OnClose
+    fun onClose() {
+        val departure = ChatMessage(MessageType.USER_LEFT, connection.pathParam("username"), null)
+        connection.broadcast().sendTextAndAwait(departure)
+    }
+
+    @OnTextMessage(broadcast = true)
+    fun onMessage(message: ChatMessage): ChatMessage {
+        return message
+    }
+}
